234e7cae214796c37c06ee622e05f3375ccc602c
Thomas Boyce <tboyce12@gmail.com>
2013-08-24 17:18:21 -0700

    Use sqlite3 database backend

--- tbsync/settings.py
+++ tbsync/settings.py
@@ -1,3 +1,8 @@
+# My settings
+
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__name__))
+
 # Django settings for tbsync project.
 
 DEBUG = True
@@ -11,13 +16,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'),
     }
 }
 
